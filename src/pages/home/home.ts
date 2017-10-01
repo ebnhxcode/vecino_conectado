081a5c8d6e66f0ee833fdda5924f4687d81d60f8
Esteban r <esteban.ramos.f@gmail.com>
2017-10-01 02:37:58 -0300

    update/add - add login ok.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, Renderer } from '@angular/core';
-import { NavController, Platform } from 'ionic-angular';
+import { NavController, Platform, NavParams } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 @Component({
@@ -10,18 +10,29 @@ export class HomePage {
 
 	splash = true;
 	tabBarElement: any;
+  data = new Array<Object>();
 
-  constructor(public platform: Platform, public renderer:Renderer,public navCtrl: NavController, public splashScreen: SplashScreen) {
+  constructor(
+    private navParams: NavParams,
+    public platform: Platform, 
+    public renderer:Renderer,
+    public navCtrl: NavController, 
+    public splashScreen: SplashScreen) {
   	//this.tabBarElement = document.querySelector('.tabbar');
-	  
+    console.log(this.navParams.get('data'));
+    this.data = this.navParams.get('data');
   }
 
 	ngAfterViewInit(){
-
   		this.platform.ready().then(() => {
 	  		this.splashScreen.hide();
 	  	});
 	}
+
+
+
+
+
   // ionViewDidLoad(){
   // 	this.tabBarElement.style.display = 'none';
   // 	setTimeout(() => {
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TabsPage } from '../tabs/tabs'
+import { HomePage } from '../home/home'
 import { 
 	AlertController, 
 	LoadingController,
@@ -19,192 +20,60 @@ import 'rxjs/add/operator/catch';
 })
 export class LoginPage{
 
-	user = {"email":"", "password":""};
+	user = {"usuario":"", "password":""};
+	data = new Array<Object>();
+	usernameOk = false;
+	passwordOk = false;
 
 	constructor(
 		private alertCtrl: AlertController, 
 		public loadingCtrl: LoadingController,
 		public navCtrl: NavController,
 		private http: Http,
-	){
+	){}
 
-		//var url = 'http://solnetjson.grown.cl/test/connection';
-		var url = 'http://local.solnetjson/test/connection';
+	ngOnInit(){ /*console.log("arranco el init");*/ }
+
+	signIn = ():void=>{ alert("signIn"); }
+
+	login = ():void=>{
+
+		var url = 'http://local.solnetjson/rest/api/get/usuarios';
+
+		let loading = this.loadingCtrl.create({
+			content:'Please wait...'
+		});
+		loading.present();
 
 		this.http.get(url)
 		.map(res => res.json())
-		.subscribe(data => {
-			console.log(data);
-		});
+		.subscribe(data => {	
 
-		/*
-		//Opcion 4 Final - POST
-		var base_uri = 'https://201.238.235.30/';
+			var self = this;
+			this.data = data.map( (d) => {
+				self.usernameOk = d.fieldData.Us_usuario == this.user.usuario ? true : false;
+				self.passwordOk = d.fieldData.Us_pass == this.user.password ? true : false;
+				/*
+				console.log(d.fieldData.Us_usuario);
+				console.log(d.fieldData.Us_pass);
+				*/
+				return d;
+			});
 
-		var service_data = {
-			'solution'	: 'Tasks_FMAngular',
-			'layout'	: 'prueba',
-			'verify'	: false
-		};
-
-		var uri_stuff = {
-			'login' 	: 'fmi/rest/api/auth/:solution',
-			'logout' 	: 'fmi/rest/api/auth/:solution',
-			'create' 	: 'fmi/rest/api/record/:solution/:layout',
-			'delete' 	: 'fmi/rest/api/record/:solution/:layout/:recordId',
-			'edit' 		: 'fmi/rest/api/record/:solution/:layout/:recordId',
-			'get' 		: 'fmi/rest/api/record/:solution/:layout/:recordId',
-			'getAll' 	: 'fmi/rest/api/record/:solution/:layout',
-		};
-
-		var json_auth = {
-			'json' : {
-				user:"nuevo",
-				password:"1234",
-				layout:"prueba"
+			if (this.usernameOk == true && this.passwordOk == true) {
+				loading.dismiss();
+				this.navCtrl.push(HomePage, {data:this.data});
+			}else{
+				let alert = this.alertCtrl.create({
+					title:'Login',
+					subTitle:'Usuario y/o contraseña invalido',
+					buttons:['Aceptar'] 					
+				});
+				loading.dismiss();
+				alert.present();
 			}
-		};
-		*/
-		/*
-		var headers = new Headers();
-    		headers.append("Accept", 'application/json');
-    		headers.append('Content-Type', 'application/json');
-    		headers.append('Access-Control-Allow-Origin', 'https://201.238.235.30/');
-    		//headers.append('Access-Control-Allow-Origin', '*');
-    		headers.append('Access-Control-Allow-Methods', 'OPTIONS, GET, HEAD, POST, DELETE');
-			headers.append('Access-Control-Allow-Credentials', 'true');
-    	/*
-		var headers = new Headers({
-			'Access-Control-Allow-Origin': 'https://201.238.235.30/',
-			'Access-Control-Allow-Credentials': 'true',
-			'Access-Control-Allow-Methods': 'OPTIONS',
-			'Access-Control-Allow-Headers': 'Content-Type, Accept',
-			'Content-Type': 'application/json',
-			'Accept': 'application/json',
-		});
-		*/
-		/*
-		var options = new RequestOptions({ headers: headers });
-		
-		this.http.post(base_uri + uri_stuff.login.replace(':solution', service_data.solution) , json_auth, options)
-			.map(response => response.json())
-			.subscribe( response => console.log(response), () => console.log('Authentication Complete') );
 
-			*/
+		});		
 
-
-
-		/*
-		//Opcion 3 - POST
-		var link = 'https://201.238.235.30/fmi/rest/api/auth/Tasks_FMAngular';
-		/*
-		let headers = new Headers({
-			//'Access-Control-Allow-Origin': 'https://201.238.235.30',
-			//'Access-Control-Allow-Credentials': 'true',
-			//'Access-Control-Allow-Methods': 'GET, POST, PUT',
-			//'Access-Control-Allow-Headers': 'Content-Type',
-			'Content-Type': 'application/json',
-			'Accept': 'application/json',
-		});
-		
-
-		var headers = new Headers();
-    	headers.append("Accept", 'application/json');
-    	headers.append('Content-Type', 'application/json');
-
-		let options = new RequestOptions({ headers: headers });
-		
-		var myData = {
-			'json' : {
-				user:"nuevo",
-				password:"1234",
-				layout:"prueba"
-			}
-		};
-		 
-		this.http.post(link, myData, options)
-			.map(response => response.json())
-			.subscribe(
-		  		response => console.log(response),
-		  		() => console.log('Authentication Complete')
-			);
-
-		*/
-		/*
-		//Opcion 2 - POST
-		let headers = new Headers({'Content-Type': 'application/json'});
-		let options = new RequestOptions({ headers: headers });
-		let url_login="https://201.238.235.30/fmi/rest/api/auth/Tasks_FMAngular";
-		let body = JSON.stringify({
-			user:"nuevo",
-			password:"1234",
-			layout:"prueba"
-		});
-		this.http.post(url_login, body, {headers: headers})
-		.map(res => res.json())
-		.subscribe(data=>{
-			console.log(data);
-		});
-		*/
-
-
-		/*
-		let url_login: string = 'https://201.238.235.30/fmi/rest/api/auth/Tasks_FMAngular';
-		let body = {
-			'user':'nuevo',
-			'password':'1234',
-			'layout':'prueba'
-		};
-
-		//Opcion 1 - POST
-		this.http.post(url_login, body, {headers: headers})
-		.map(response => response.json())
-		.subscribe(
-		  response => console.log(response),
-		  () => console.log('Authentication Complete')
-		);
-		*/
-
-		/*
-		// Url para probar metodo GET con angular
-		//let url: string = 'https://jsonplaceholder.typicode.com/posts';
-		let url: string = 'https://201.238.235.30/fmi/rest/api/record/Tasks_FMAngular/prueba';
-
-		//Opcion 1 - GET
-		this.http.get(url)
-		.map(res => res.json())
-		.subscribe(data => {
-			console.log(data);
-		});
-		*/
-
-		/*
-		this.http.post('http://localhost:3001/sessions/create', body, { headers: headers })
-		.map(response => response.json())
-		.subscribe(
-		  response => this.storeToken(response.id_token),
-		  this.logError,
-		  () => console.log('Authentication Complete')
-		);
-		*/
-
-		//Opcion 2 - GET
-		/*
-		this.http.get(url)
-		.do(res => res.json()).
-		subscribe(data => {
-			console.log(data)
-		});
-		*/
-	}
-
-	ngOnInit(){
-		console.log("arranco el init");
-	}
-
-
-	signIn = ():void=>{
-		alert("signIn");
-	}
-
+	} 
 }
